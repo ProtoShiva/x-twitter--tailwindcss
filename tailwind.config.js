8a05b81e1a9b4b63aae726404656d947d6a11137
Himanshu <hgusain088@gmail.com>
2024-02-09 21:54:50 +0530

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["*html"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        "sans": ["Roboto", "Helvetica", "Arial", "sans-serif"],
+        "xFont-r": ["xFont-r"],
+        "xFont-b": ["xFont-b"],
+        "xFont-h": ["xFont-h"],
+        "xFont-m": ["xFont-m"]
+      }
+    }
   },
   plugins: []
 }
